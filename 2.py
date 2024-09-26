df5677bb237316d3ee908896ac7eeacb15a3790a
Taras-ops <baraniuktaras098@gmail.com>
2024-09-26 16:13:58 +0300

    correct spelling

--- 2.py
+++ 2.py
@@ -1,4 +1,4 @@
-def fahreheit_to_celsius(t):
+def fahrenheit_to_celsius(t):
     return round((t - 32) / 1.8, 2)
 
 
@@ -6,12 +6,12 @@ def transform_temperatures():
     temperatures_celsius = []
 
     for i in range(7):
-        temperature = input('Write the temperature in Fahrenheit degreees of the ' + str(i + 1) + ' day of the week: ')
+        temperature = input('Write the temperature in Fahrenheit degrees of the ' + str(i + 1) + ' day of the week: ')
 
         while not temperature.isdigit():
             temperature = input('Please, Write the temperature in Fahrenheit degreees of the ' + str(i + 1) + ' day of the week: ')
 
-        temperature_celsius = fahreheit_to_celsius(int(temperature))
+        temperature_celsius = fahrenheit_to_celsius(int(temperature))
 
         if temperature_celsius < 10:
             print('\n' + 'Cold!' + '\n')
@@ -29,9 +29,9 @@ def transform_temperatures():
     max_temperature = max(temperatures_celsius)
     min_temperature = min(temperatures_celsius)
 
-    print(f'Average temperature in Celcius degree: {average_temperature}')
-    print(f'Max temperature in Celcius degree: {max_temperature}')
-    print(f'Min temperature in Celcius degree: {min_temperature}')
+    print(f'Average temperature in Celsius degree: {average_temperature}')
+    print(f'Max temperature in Celsius degree: {max_temperature}')
+    print(f'Min temperature in Celsius degree: {min_temperature}')
 
 
 transform_temperatures()
